0bbc260306bd1af9a6a0c3559d9320d2716039bf
hello <hello@hello.com>
2025-05-04 20:45:50 -0400

    Update Next.js configuration for Cloudflare Pages

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
   swcMinify: true,
   output: 'export',
   trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config) => {
+    config.output.publicPath = '/_next/';
+    return config;
+  },
 }
 
 module.exports = nextConfig
